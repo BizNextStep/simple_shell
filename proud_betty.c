6670edbcc3f881b9c57c59497d2f1f9ad73d1369
Ademola-1 <lawalademola71@gmail.com>
2023-11-07 00:13:46 -0800

    A beautiful code that passes the Betty checks

---
+++ proud_betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("We are team of two:\n")
+	printf("Mubarak Lawal and Charmaine Mbatha\n");
+	printf("Working on a simple shell project.\n");
+
+	return (0);
+}
